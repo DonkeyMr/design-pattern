c1782ee1e3c3f8cb28187d2758b2a82c30d2beab
MrDonkey <1532129326@qq.com>
2020-11-05 20:55:26 +0800

    chain of Responsibility pattern

---
+++ src/com/pattern/chain_of_responsibility/AbstractLogger.java
@@ -0,0 +1,31 @@
+package com.pattern.chain_of_responsibility;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+public abstract class AbstractLogger {
+
+    public static int INFO = 1;
+
+    public static int DEBUG = 2;
+
+    public static int ERROR = 3;
+
+    protected int level;
+
+    protected AbstractLogger nextLogger;
+
+    public void setNextLogger(AbstractLogger nextLogger) {
+        this.nextLogger = nextLogger;
+    }
+
+    public void logMessage(int level, String message) {
+        if (this.level <= level) {
+            write(message);
+        }
+        if (nextLogger != null) {
+            nextLogger.logMessage(level, message);
+        }
+    }
+
+    abstract void write(String message);
+}
---
+++ src/com/pattern/chain_of_responsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package com.pattern.chain_of_responsibility;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
